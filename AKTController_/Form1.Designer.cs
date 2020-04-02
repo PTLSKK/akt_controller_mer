10d0b5236df99552b121f8173fccf3343bed7d77
Vicky <63042294+vickylskk@users.noreply.github.com>
2020-04-02 21:12:50 +0700

    another files
    another files

---
+++ AKTController_/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace AKTController_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.M1_Stop_Rotate = new System.Windows.Forms.Button();
+            this.M1_Stop_Tilt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.M1_Rgt3 = new System.Windows.Forms.Button();
+            this.M1_Lft3 = new System.Windows.Forms.Button();
+            this.M1_Dwn3 = new System.Windows.Forms.Button();
+            this.M1_Up3 = new System.Windows.Forms.Button();
+            this.M1_Rgt2 = new System.Windows.Forms.Button();
+            this.M1_Lft2 = new System.Windows.Forms.Button();
+            this.M1_Dwn2 = new System.Windows.Forms.Button();
+            this.M1_Rgt1 = new System.Windows.Forms.Button();
+            this.M1_Up2 = new System.Windows.Forms.Button();
+            this.M1_Up1 = new System.Windows.Forms.Button();
+            this.M1_J = new System.Windows.Forms.Button();
+            this.M1_Lft1 = new System.Windows.Forms.Button();
+            this.M1_Dwn1 = new System.Windows.Forms.Button();
+            this.M1_S = new System.Windows.Forms.Button();
+            this.M1_R = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel1.Controls.Add(this.M1_Stop_Rotate);
+            this.panel1.Controls.Add(this.M1_Stop_Tilt);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.M1_Rgt3);
+            this.panel1.Controls.Add(this.M1_Lft3);
+            this.panel1.Controls.Add(this.M1_Dwn3);
+            this.panel1.Controls.Add(this.M1_Up3);
+            this.panel1.Controls.Add(this.M1_Rgt2);
+            this.panel1.Controls.Add(this.M1_Lft2);
+            this.panel1.Controls.Add(this.M1_Dwn2);
+            this.panel1.Controls.Add(this.M1_Rgt1);
+            this.panel1.Controls.Add(this.M1_Up2);
+            this.panel1.Controls.Add(this.M1_Up1);
+            this.panel1.Controls.Add(this.M1_J);
+            this.panel1.Controls.Add(this.M1_Lft1);
+            this.panel1.Controls.Add(this.M1_Dwn1);
+            this.panel1.Controls.Add(this.M1_S);
+            this.panel1.Controls.Add(this.M1_R);
+            this.panel1.Location = new System.Drawing.Point(41, 71);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 254);
+            this.panel1.TabIndex = 0;
+            // 
+            // M1_Stop_Rotate
+            // 
+            this.M1_Stop_Rotate.Location = new System.Drawing.Point(247, 193);
+            this.M1_Stop_Rotate.Name = "M1_Stop_Rotate";
+            this.M1_Stop_Rotate.Size = new System.Drawing.Size(124, 32);
+            this.M1_Stop_Rotate.TabIndex = 17;
+            this.M1_Stop_Rotate.Text = "Stop Rotate";
+            this.M1_Stop_Rotate.UseVisualStyleBackColor = true;
+            this.M1_Stop_Rotate.Click += new System.EventHandler(this.M1_Stop_Rotate_Click);
+            // 
+            // M1_Stop_Tilt
+            // 
+            this.M1_Stop_Tilt.Location = new System.Drawing.Point(117, 193);
+            this.M1_Stop_Tilt.Name = "M1_Stop_Tilt";
+            this.M1_Stop_Tilt.Size = new System.Drawing.Size(124, 32);
+            this.M1_Stop_Tilt.TabIndex = 16;
+            this.M1_Stop_Tilt.Text = "Stop Tilt";
+            this.M1_Stop_Tilt.UseVisualStyleBackColor = true;
+            this.M1_Stop_Tilt.Click += new System.EventHandler(this.M1_Stop_Tilt_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Meriam 1";
+            // 
+            // M1_Rgt3
+            // 
+            this.M1_Rgt3.Location = new System.Drawing.Point(312, 145);
+            this.M1_Rgt3.Name = "M1_Rgt3";
+            this.M1_Rgt3.Size = new System.Drawing.Size(59, 32);
+            this.M1_Rgt3.TabIndex = 14;
+            this.M1_Rgt3.Text = "Rgt3";
+            this.M1_Rgt3.UseVisualStyleBackColor = true;
+            this.M1_Rgt3.Click += new System.EventHandler(this.M1_Rgt3_Click);
+            // 
+            // M1_Lft3
+            // 
+            this.M1_Lft3.Location = new System.Drawing.Point(247, 145);
+            this.M1_Lft3.Name = "M1_Lft3";
+            this.M1_Lft3.Size = new System.Drawing.Size(59, 32);
+            this.M1_Lft3.TabIndex = 13;
+            this.M1_Lft3.Text = "Lft3";
+            this.M1_Lft3.UseVisualStyleBackColor = true;
+            this.M1_Lft3.Click += new System.EventHandler(this.M1_Lft3_Click);
+            // 
+            // M1_Dwn3
+            // 
+            this.M1_Dwn3.Location = new System.Drawing.Point(182, 145);
+            this.M1_Dwn3.Name = "M1_Dwn3";
+            this.M1_Dwn3.Size = new System.Drawing.Size(59, 32);
+            this.M1_Dwn3.TabIndex = 12;
+            this.M1_Dwn3.Text = "Dwn3";
+            this.M1_Dwn3.UseVisualStyleBackColor = true;
+            this.M1_Dwn3.Click += new System.EventHandler(this.M1_Dwn3_Click);
+            // 
+            // M1_Up3
+            // 
+            this.M1_Up3.Location = new System.Drawing.Point(117, 145);
+            this.M1_Up3.Name = "M1_Up3";
+            this.M1_Up3.Size = new System.Drawing.Size(59, 32);
+            this.M1_Up3.TabIndex = 11;
+            this.M1_Up3.Text = "Up3";
+            this.M1_Up3.UseVisualStyleBackColor = true;
+            this.M1_Up3.Click += new System.EventHandler(this.M1_Up3_Click);
+            // 
+            // M1_Rgt2
+            // 
+            this.M1_Rgt2.Location = new System.Drawing.Point(312, 107);
+            this.M1_Rgt2.Name = "M1_Rgt2";
+            this.M1_Rgt2.Size = new System.Drawing.Size(59, 32);
+            this.M1_Rgt2.TabIndex = 10;
+            this.M1_Rgt2.Text = "Rgt2";
+            this.M1_Rgt2.UseVisualStyleBackColor = true;
+            this.M1_Rgt2.Click += new System.EventHandler(this.M1_Rgt2_Click);
+            // 
+            // M1_Lft2
+            // 
+            this.M1_Lft2.Location = new System.Drawing.Point(247, 107);
+            this.M1_Lft2.Name = "M1_Lft2";
+            this.M1_Lft2.Size = new System.Drawing.Size(59, 32);
+            this.M1_Lft2.TabIndex = 9;
+            this.M1_Lft2.Text = "Lft2";
+            this.M1_Lft2.UseVisualStyleBackColor = true;
+            this.M1_Lft2.Click += new System.EventHandler(this.M1_Lft2_Click);
+            // 
+            // M1_Dwn2
+            // 
+            this.M1_Dwn2.Location = new System.Drawing.Point(182, 107);
+            this.M1_Dwn2.Name = "M1_Dwn2";
+            this.M1_Dwn2.Size = new System.Drawing.Size(59, 32);
+            this.M1_Dwn2.TabIndex = 8;
+            this.M1_Dwn2.Text = "Dwn2";
+            this.M1_Dwn2.UseVisualStyleBackColor = true;
+            this.M1_Dwn2.Click += new System.EventHandler(this.M1_Dwn2_Click);
+            // 
+            // M1_Rgt1
+            // 
+            this.M1_Rgt1.Location = new System.Drawing.Point(312, 69);
+            this.M1_Rgt1.Name = "M1_Rgt1";
+            this.M1_Rgt1.Size = new System.Drawing.Size(59, 32);
+            this.M1_Rgt1.TabIndex = 7;
+            this.M1_Rgt1.Text = "Rgt1";
+            this.M1_Rgt1.UseVisualStyleBackColor = true;
+            this.M1_Rgt1.Click += new System.EventHandler(this.M1_Rgt1_Click);
+            // 
+            // M1_Up2
+            // 
+            this.M1_Up2.Location = new System.Drawing.Point(117, 107);
+            this.M1_Up2.Name = "M1_Up2";
+            this.M1_Up2.Size = new System.Drawing.Size(59, 32);
+            this.M1_Up2.TabIndex = 6;
+            this.M1_Up2.Text = "Up2";
+            this.M1_Up2.UseVisualStyleBackColor = true;
+            this.M1_Up2.Click += new System.EventHandler(this.M1_Up2_Click);
+            // 
+            // M1_Up1
+            // 
+            this.M1_Up1.Location = new System.Drawing.Point(117, 69);
+            this.M1_Up1.Name = "M1_Up1";
+            this.M1_Up1.Size = new System.Drawing.Size(59, 32);
+            this.M1_Up1.TabIndex = 5;
+            this.M1_Up1.Text = "Up1";
+            this.M1_Up1.UseVisualStyleBackColor = true;
+            this.M1_Up1.Click += new System.EventHandler(this.M1_Up1_Click);
+            // 
+            // M1_J
+            // 
+            this.M1_J.Location = new System.Drawing.Point(34, 107);
+            this.M1_J.Name = "M1_J";
+            this.M1_J.Size = new System.Drawing.Size(59, 32);
+            this.M1_J.TabIndex = 4;
+            this.M1_J.Text = "J";
+            this.M1_J.UseVisualStyleBackColor = true;
+            this.M1_J.Click += new System.EventHandler(this.M1_J_Click);
+            // 
+            // M1_Lft1
+            // 
+            this.M1_Lft1.Location = new System.Drawing.Point(247, 69);
+            this.M1_Lft1.Name = "M1_Lft1";
+            this.M1_Lft1.Size = new System.Drawing.Size(59, 32);
+            this.M1_Lft1.TabIndex = 3;
+            this.M1_Lft1.Text = "Lft1";
+            this.M1_Lft1.UseVisualStyleBackColor = true;
+            this.M1_Lft1.Click += new System.EventHandler(this.M1_Lft1_Click);
+            // 
+            // M1_Dwn1
+            // 
+            this.M1_Dwn1.Location = new System.Drawing.Point(182, 69);
+            this.M1_Dwn1.Name = "M1_Dwn1";
+            this.M1_Dwn1.Size = new System.Drawing.Size(59, 32);
+            this.M1_Dwn1.TabIndex = 2;
+            this.M1_Dwn1.Text = "Dwn1";
+            this.M1_Dwn1.UseVisualStyleBackColor = true;
+            this.M1_Dwn1.Click += new System.EventHandler(this.M1_Dwn1_Click);
+            // 
+            // M1_S
+            // 
+            this.M1_S.Location = new System.Drawing.Point(34, 145);
+            this.M1_S.Name = "M1_S";
+            this.M1_S.Size = new System.Drawing.Size(59, 32);
+            this.M1_S.TabIndex = 1;
+            this.M1_S.Text = "S";
+            this.M1_S.UseVisualStyleBackColor = true;
+            this.M1_S.Click += new System.EventHandler(this.M1_S_Click);
+            // 
+            // M1_R
+            // 
+            this.M1_R.Location = new System.Drawing.Point(34, 69);
+            this.M1_R.Name = "M1_R";
+            this.M1_R.Size = new System.Drawing.Size(59, 32);
+            this.M1_R.TabIndex = 0;
+            this.M1_R.Text = "R";
+            this.M1_R.UseVisualStyleBackColor = true;
+            this.M1_R.Click += new System.EventHandler(this.M1_R_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(474, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(340, 25);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "AKT-MERIAM57 CONTROLLER";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "AKT-MERIAM57 SENDER";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button M1_Rgt3;
+        private System.Windows.Forms.Button M1_Lft3;
+        private System.Windows.Forms.Button M1_Dwn3;
+        private System.Windows.Forms.Button M1_Up3;
+        private System.Windows.Forms.Button M1_Rgt2;
+        private System.Windows.Forms.Button M1_Lft2;
+        private System.Windows.Forms.Button M1_Dwn2;
+        private System.Windows.Forms.Button M1_Rgt1;
+        private System.Windows.Forms.Button M1_Up2;
+        private System.Windows.Forms.Button M1_Up1;
+        private System.Windows.Forms.Button M1_J;
+        private System.Windows.Forms.Button M1_Lft1;
+        private System.Windows.Forms.Button M1_Dwn1;
+        private System.Windows.Forms.Button M1_S;
+        private System.Windows.Forms.Button M1_R;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button M1_Stop_Rotate;
+        private System.Windows.Forms.Button M1_Stop_Tilt;
+    }
+}
+
---
+++ AKTController_/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace AKTController_
+{
+    public partial class Form1 : Form
+    {
+        IPEndPoint endM1;
+        UdpClient udpM1;
+        byte[] bytesM1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Meriam 1
+            udpM1 = new UdpClient();
+            endM1 = new IPEndPoint(IPAddress.Parse("192.168.1.16"), 1016);
+            udpM1.Connect(endM1);
+            
+        }
+
+        
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            udpM1.Close();
+        }
+
+        private void M1_R_Click(object sender, EventArgs e)
+        {
+            bytesM1 = Encoding.ASCII.GetBytes("R");
+            udpM1.Send(bytesM1, bytesM1.Length);
+        }
+
+        private void M1_S_Click(object sender, EventArgs e)
+        {
+            bytesM1 = Encoding.ASCII.GetBytes("S");
+            udpM1.Send(bytesM1, bytesM1.Length);
+        }
+
+        private void M1_J_Click(object sender, EventArgs e)
+        {
+            bytesM1 = Encoding.ASCII.GetBytes("J");
+            udpM1.Send(bytesM1, bytesM1.Length);
+        }
+
+        private void M1_Up1_Click(object sender, EventArgs e)
+        {
+            bytesM1 = Encoding.ASCII.GetBytes("U1");
+            udpM1.Send(bytesM1, bytesM1.Length);
+        }
+
+        private void M1_Dwn1_Click(object sender, EventArgs e)
+        {
+            bytesM1 = Encoding.ASCII.GetBytes("D1");
+            udpM1.Send(bytesM1, bytesM1.Length);
+        }
+
+        private void M1_Lft1_Click(object sender, EventArgs e)
+        {
+            bytesM1 = Encoding.ASCII.GetBytes("L1");
+            udpM1.Send(bytesM1, bytesM1.Length);
+        }
+
+        private void M1_Rgt1_Click(object sender, EventArgs e)
+        {
+            bytesM1 = Encoding.ASCII.GetBytes("R1");
+            udpM1.Send(bytesM1, bytesM1.Length);
+        }
+
+        private void M1_Up2_Click(object sender, EventArgs e)
+        {
+            bytesM1 = Encoding.ASCII.GetBytes("U2");
+            udpM1.Send(bytesM1, bytesM1.Length);
+        }
+
+        private void M1_Dwn2_Click(object sender, EventArgs e)
+        {
+            bytesM1 = Encoding.ASCII.GetBytes("D2");
+            udpM1.Send(bytesM1, bytesM1.Length);
+        }
+
+        private void M1_Lft2_Click(object sender, EventArgs e)
+        {
+            bytesM1 = Encoding.ASCII.GetBytes("L2");
+            udpM1.Send(bytesM1, bytesM1.Length);
+        }
+
+        private void M1_Rgt2_Click(object sender, EventArgs e)
+        {
+            bytesM1 = Encoding.ASCII.GetBytes("R2");
+            udpM1.Send(bytesM1, bytesM1.Length);
+        }
+
+        private void M1_Up3_Click(object sender, EventArgs e)
+        {
+            bytesM1 = Encoding.ASCII.GetBytes("U3");
+            udpM1.Send(bytesM1, bytesM1.Length);
+        }
+
+        private void M1_Dwn3_Click(object sender, EventArgs e)
+        {
+            bytesM1 = Encoding.ASCII.GetBytes("D3");
+            udpM1.Send(bytesM1, bytesM1.Length);
+        }
+
+        private void M1_Lft3_Click(object sender, EventArgs e)
+        {
+            bytesM1 = Encoding.ASCII.GetBytes("L3");
+            udpM1.Send(bytesM1, bytesM1.Length);
+        }
+
+        private void M1_Rgt3_Click(object sender, EventArgs e)
+        {
+            bytesM1 = Encoding.ASCII.GetBytes("R3");
+            udpM1.Send(bytesM1, bytesM1.Length);
+        }
+
+        private void M1_Stop_Tilt_Click(object sender, EventArgs e)
+        {
+            bytesM1 = Encoding.ASCII.GetBytes("ST1");
+            udpM1.Send(bytesM1, bytesM1.Length);
+        }
+
+        private void M1_Stop_Rotate_Click(object sender, EventArgs e)
+        {
+            bytesM1 = Encoding.ASCII.GetBytes("SR1");
+            udpM1.Send(bytesM1, bytesM1.Length);
+        }
+    }
+}
